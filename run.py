5c4dcb663101ff6fd18a6f6896e240f7446ba1a7
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-06-13 16:17:32 -0700

    db bug

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ db.init_app(app)
 
 @app.before_first_request
 def create_tables():
+    db.drop_all()
     db.create_all()
     db.engine.execute(UserModel.__table__.insert(), users)
     db.engine.execute(BeerModel.__table__.insert(), beers)
